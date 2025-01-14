92efaad95d11b1dca0173ac8e7e62305a1aa23de
MrSharkey17 <123600203+MrSharkey17@users.noreply.github.com>
2025-01-13 23:20:50 -0500

    Sprint Removed and Walking Anims added

--- Puzzle Game/Assets/Scripts/PlayerController.cs
+++ Puzzle Game/Assets/Scripts/PlayerController.cs
@@ -8,32 +8,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Animator animator;
 
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float runSpeed;
     public Vector3 playerMoveDirection;
-
     void Update()
     {
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
         playerMoveDirection = new Vector3(inputX, inputY).normalized;
 
-         bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float currentSpeed = isRunning ? runSpeed : moveSpeed;
-
-        rb.linearVelocity = playerMoveDirection * currentSpeed;
-
+        rb.linearVelocity = playerMoveDirection * moveSpeed;
         animator.SetFloat("moveX", inputX);
         animator.SetFloat("moveY", inputY);
-        animator.SetFloat("speed", rb.linearVelocity.magnitude);
-        animator.SetBool("isRunning", isRunning);
         if (playerMoveDirection == Vector3.zero){
             animator.SetBool("moving", false);
         } else {
             animator.SetBool("moving", true);
         }
-    }
-    void FixedUpdate(){
-        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
-        rb.linearVelocity = new Vector2(playerMoveDirection.x * currentSpeed, playerMoveDirection.y * currentSpeed);
     }
 }
